52c9cfb39ff8cfb6a2b4c6a4f2a386a3fe2bc02d
Sam Raab <raabsm@gmail.com>
2020-02-24 14:51:19 -0500

    highlights nyc zip codes on mouseover

--- webapp.py
+++ webapp.py
@@ -1,15 +1,17 @@
 import tornado.ioloop
 import tornado.web
 import api_keys
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('map.html', API_KEY=api_keys.Google_Maps_API_KEY)
+        self.render('geo_map.html', API_KEY=api_keys.Google_Maps_API_KEY)
 
 
 def make_app():
     return tornado.web.Application([
                     (r"/", MainHandler),
+                    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/"}),
                         ])
 
 
