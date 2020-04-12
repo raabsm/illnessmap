7fbcab1e05a80956775118be0f4f2a4f2fc913e4
Sam Raab <raabsm@gmail.com>
2020-04-12 14:53:13 -0400

    added basic restaurant page

--- webapp.py
+++ webapp.py
@@ -8,9 +8,30 @@ class MainHandler(tornado.web.RequestHandler):
         self.render('geo_map.html', API_KEY=api_keys.Google_Maps_API_KEY)
 
 
+class RestaurantHandler(tornado.web.RequestHandler):
+    def post(self):
+        business_id = self.get_argument('business_id')
+        start_time = self.get_argument('start_time')
+        end_time = self.get_argument('end_time')
+        review_bottom_threshold = self.get_argument('review_bottom_threshold')
+        review_top_threshold = self.get_argument('review_top_threshold')
+        print(business_id, start_time, end_time, review_bottom_threshold, review_top_threshold)
+        self.render('restaurant.html', start_time=start_time,
+                                        end_time=end_time,
+                                        review_bottom_threshold=review_bottom_threshold,
+                                        review_top_threshold=review_top_threshold,
+                                        business_id=business_id)
+
+class test(tornado.web.RequestHandler):
+    def get(self):
+        self.render('map.html', API_KEY=api_keys.Google_Maps_API_KEY)
+
+
 def make_app():
     return tornado.web.Application([
                     (r"/", MainHandler),
+                    (r"/restaurant_info", RestaurantHandler),
+                    (r"/test", test),
                     (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/"}),
                         ])
 
