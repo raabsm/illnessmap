5a5b37f38e56fd083f63c7e5cb1aaadb6468d961
Sam Raab <raabsm@gmail.com>
2020-05-04 11:03:42 -0400

    automated backend, still slow on initial query

--- webapp.py
+++ webapp.py
@@ -3,8 +3,11 @@ import tornado.web
 import api_keys
 import json
 from mongodb_connection import MongoConnection
-from bson.objectid import ObjectId
 from bson.json_util import dumps
+import time
+import schedule
+
+global data, db
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -19,11 +22,12 @@ class RestaurantHandler(tornado.web.RequestHandler):
         end_time = self.get_argument('end_time')
         review_bottom_threshold = self.get_argument('review_bottom_threshold')
         review_top_threshold = self.get_argument('review_top_threshold')
-        self.render('restaurant.html', start_time=start_time,
-                                        end_time=end_time,
-                                        review_bottom_threshold=review_bottom_threshold,
-                                        review_top_threshold=review_top_threshold,
-                                        business_id=business_id)
+        self.render('restaurant.html',
+                    start_time=start_time,
+                    end_time=end_time,
+                    review_bottom_threshold=review_bottom_threshold,
+                    review_top_threshold=review_top_threshold,
+                    business_id=business_id)
 
 
 class GetRestuarantInfo(tornado.web.RequestHandler):
@@ -33,30 +37,51 @@ class GetRestuarantInfo(tornado.web.RequestHandler):
         self.write(info)
 
 
-def make_app():
-    return tornado.web.Application([
-                    (r"/", MainHandler),
-                    (r"/restaurant_info", RestaurantHandler),
-                    (r"/get_restaurant_info", GetRestuarantInfo),
-                    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/"}),
-                    (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/images"}),
-                    (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/research/css/"})
-                        ])
+class GetReviews(tornado.web.RequestHandler):
+    def get(self):
+        self.write(data)
+        schedule.run_pending()
+
+
+def get_newest_reviews():
+    print("Querying DB...")
+    global data
+    collection = db.return_collection(api_keys.DB_INFO['collections']['condensed_reviews'])
+    data = collection.find({})
+    data = dumps(data)
+    print("Data updated")
+
+
+schedule.every().day.do(get_newest_reviews)
 
 
 def init_mongo():
     global db
-    db = MongoConnection('FoodIllness', api_keys.MongoDBURI)
+    db = MongoConnection(api_keys.DB_INFO['db'], api_keys.DB_INFO['uri'])
 
 
 def get_rest_info(id):
-    collection = db.return_collection('MergedRestaurantReviews')
+    collection = db.return_collection(api_keys.DB_INFO['collections']['all_reviews'])
     rest_info = collection.find_one({'_id': id})
     return rest_info
 
 
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/restaurant_info", RestaurantHandler),
+        (r"/get_reviews", GetReviews),
+        (r"/get_restaurant_info", GetRestuarantInfo),
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/"}),
+        (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/datafiles/images"}),
+        (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "/home/smr2218/research/css/"})
+    ])
+
+
 if __name__ == "__main__":
     init_mongo()
+    get_newest_reviews()
     app = make_app()
     app.listen(9898)
+    print('running')
     tornado.ioloop.IOLoop.current().start()
